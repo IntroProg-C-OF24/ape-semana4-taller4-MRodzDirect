b6087bb985db5031507f3c6dd1285c0f6a6060be
MRodz <mabarca1@utpl.edu.ec>
2023-11-01 16:49:01 -0500

    Ej3-prog1

---
+++ APE_Week4_Solve/src/ape_week4_solve/TablaMult2al6_3.java
@@ -0,0 +1,7 @@
+package ape_week4_solve;
+import java.util.Scanner;
+public class TablaMult2al6_3 {
+    public static void main(String[] args) {
+        int num1, mult1, mult2, mult3, mult4, mult5, mult6, mult7, mult8, mult9, mult10, mult11, mult12, mult13;
+    }
+}
