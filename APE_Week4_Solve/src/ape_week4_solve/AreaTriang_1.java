740a5ceb7ad55133091790cc088f6e3955ca9e37
MRodz <mabarca1@utpl.edu.ec>
2023-11-01 16:25:19 -0500

    Lesto ;)

---
+++ APE_Week4_Solve/src/ape_week4_solve/AreaTriang_1.java
@@ -0,0 +1,31 @@
+/*
+Algoritmo-1
+
+Generar un algoritmo que permite calcular y presentar el área de un triángulo. 
+Los datos deben ser pedidos al usuario.
+ */
+
+package ape_week4_solve;
+import java.util.Scanner;
+import java.util.*;
+public class AreaTriang_1 {
+
+    public static void main(String[] args) {
+     double baseTri;
+     double altTri;
+     double areaTri;
+     Scanner sc = new Scanner(System.in); // new scanner (System.in) = Declarar e Inicializar
+        System.out.println("Ingresar la base del triangulo: ");
+     baseTri = sc.nextDouble();
+     System.out.println("Ingresar la altura del triangulo: ");
+     altTri = sc.nextDouble();
+     
+     areaTri = (baseTri*altTri)/2 ;
+        System.out.println("La area del triangulo es: "+areaTri);
+    }
+    /**
+     * Si lo que se declara comienza con: Mayuscula = objeto / Minuscula = variable
+     * ALT+SHIFT+F = INDENTACION o Format
+     * Algoritmo-1
+     */
+}
