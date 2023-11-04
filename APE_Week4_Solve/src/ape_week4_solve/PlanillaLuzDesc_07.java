4c1aa034962922e1e5d48ef0a98ccab99978dba1
MRodzDirect-Original <mabarca1@utpl.edu.ec>
2023-11-04 18:58:23 -0500

    Ej7

---
+++ APE_Week4_Solve/src/ape_week4_solve/PlanillaLuzDesc_07.java
@@ -0,0 +1,28 @@
+package ape_week4_solve;
+import java.util.Scanner;
+public class PlanillaLuzDesc_07 {
+    public static void main(String[] args) {
+    int desc, edad;
+    double kilHo, kilSpend, totalDesc, totalNoDesc;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Ingrese el $ costo de cada kilovatio por hora segun su planilla de Luz");
+    kilHo = sc.nextDouble();
+    System.out.println("Ingrese la cantidad de kilovatios que ha consumido este mes segun su planilla de Luz");
+    kilSpend = sc.nextDouble();
+    System.out.println("Ingrese su edad");
+    edad = sc.nextInt();
+    if (edad>=65){
+    System.out.println("Ingrese el descuento (%) que ofrece su plan por mayoria de edad");
+    desc = sc.nextInt();
+    totalDesc = (kilHo*kilSpend)*(1-(desc*0.01));
+        System.out.println("El costo de su planilla de Luz es "+ totalDesc + "$");
+    // Alternative: totalDesc = (kilHo*kilSpend)-(kilHo*kilSpend*desc)
+    } 
+    else {
+    totalNoDesc= kilHo * kilSpend;
+    System.out.println("El costo de su planilla de Luz es "+ totalNoDesc + "$");
+            }
+    
+    
+    }
+}
